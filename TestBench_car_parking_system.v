24d21951d85417b32df41d55dadc3dfa104a5cde
notlongbottom <ananyamadaan0607@gmail.com>
2023-08-04 00:30:12 +0530

    UUT

--- TestBench_car_parking_system.v
+++ TestBench_car_parking_system.v
@@ -15,3 +15,21 @@ module tb_parking_system;
   wire RED_LED;
   wire [6:0] HEX_1;
   wire [6:0] HEX_2;
+
+// Instantiate the Unit Under Test (UUT)
+  parking_system uut (
+  .clk(clk), 
+  .reset_n(reset_n), 
+  .sensor_entrance(sensor_entrance), 
+  .sensor_exit(sensor_exit), 
+  .password_1(password_1), 
+  .password_2(password_2), 
+  .GREEN_LED(GREEN_LED), 
+  .RED_LED(RED_LED), 
+  .HEX_1(HEX_1), 
+ .HEX_2(HEX_2)
+ );
+ initial begin
+ clk = 0;
+ forever #10 clk = ~clk;
+ end

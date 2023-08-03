ba8ccd4af952ea5d380c9f3b91b5145ac53ba313
notlongbottom <ananyamadaan0607@gmail.com>
2023-08-04 00:24:53 +0530

    inputs

---
+++ TestBench_car_parking_system.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module tb_parking_system;
+
+  // Inputs
+  reg clk;
+  reg reset_n;
+  reg sensor_entrance;
+  reg sensor_exit;
+  reg [1:0] password_1;
+  reg [1:0] password_2;
+
+ 
